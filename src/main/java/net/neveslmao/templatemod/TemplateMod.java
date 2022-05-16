78f5c95a99130f129c8bd6356ba1562dd8a8e1c0
neveslmao <iuriayolphineves@gmail.com>
2022-05-16 11:55:19 +0100

    Initial commit

---
+++ src/main/java/net/neveslmao/templatemod/TemplateMod.java
@@ -0,0 +1,14 @@
+package net.neveslmao.templatemod;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TemplateMod implements ModInitializer {
+	public static final String MOD_ID = "templatemod";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
